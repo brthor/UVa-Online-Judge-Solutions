481dcc1aece60b1f3a1f045724b254b1cd779934
Mozoby <brthornbury@csupomona.edu>
2014-10-27 18:35:35 -0700

    Segment Tree working implementation 11235

--- 11235.cpp
+++ 11235.cpp
@@ -7,112 +7,127 @@
 */
 
 #include <vector>
-#include <math.h>
+#include <cmath>
 #include <stdio.h>
 
 using namespace std;
 
 typedef pair<int, int> ii;
-typedef pair<int, ii> iii;
+typedef vector<ii > vii;
 
+#define MAX 200000
 #define log2(n) log(double(n))/log(2.0)
-#define MAX 200005
 #define REP(i,a,b) for(int i = int(a); i<=int(b); ++i)
 
 int start[MAX];
-int ends[MAX];
-int freq[MAX];
-int actual[MAX];
+int elemEnd[MAX];
 
-vector<int > segmentTree; //index of max
+vector<int> sTree;
 
-void initSegmentTree(int _size){
-	int tSize = 2*pow(2, floor(log2(_size)) + 1);
-	segmentTree.resize(tSize, 0);
+void initTree(int _size){
+	int count = 2*pow(2,floor(log2(_size)) + 1);
+	sTree.resize(count);
 }
 
-void buildSegmentTree(int arr[], int node, int left, int right){
-	if(left == right){
-		segmentTree[node] = left;
-	}else{
-		//recursively find left and right subtrees (2*i + 2*i+1 nodes)
-		//go left -> left = left, right = left+right/2
-		//go right -> right = right, left = left+right/2 + 1
-
-		int leftIndex = 2*node;
-		int rightIndex = 2* node + 1;
-		int mid = (left+right)/2;
-
-		buildSegmentTree(arr, 2*node, left, mid);
-		buildSegmentTree(arr, 2*node +1, mid+1, right);
-
-		int leftContent = arr[leftIndex];
-		int rightContent = arr[rightIndex];
-
-		segmentTree[node] = arr[segmentTree[2*node+1]] > arr[segmentTree[2*node]] ? segmentTree[2*node+1] : segmentTree[2*node];
+void buildTree(vector<int> &values, int lb, int rb, int node){
+	if(lb == rb){
+		sTree[node] = lb;
+		return;
 	}
+
+	int mid = (lb + rb)/2;
+
+	int leftIndex = node*2;
+	int rightIndex = node*2 + 1;
+
+	buildTree(values, lb, mid, leftIndex);
+	buildTree(values, mid+1, rb, rightIndex);
+
+	int leftValue = values[sTree[leftIndex]];
+	int rightValue = values[sTree[rightIndex]];
+
+	sTree[node] = leftValue > rightValue ? sTree[leftIndex] : sTree[rightIndex];
 }
 
-int queryTree(int arr[], int rl, int rr, int node, int left, int right){
-	if(left > rr || right < rl) return -1;
-	if(left >= rl && right <= rr) return segmentTree[node];
+int rangeMaxQuery(vector<int> &values, int rangeLeft, int rangeRight, int searchLeft, int searchRight, int node){
+	if(rangeLeft > searchRight || rangeRight < searchLeft) return -1;
+	if(searchLeft >= rangeLeft && searchRight <= rangeRight) return sTree[node];
 
-	int mid = (left + right)/2;
+	int mid = (searchLeft + searchRight)/2;
 
-	int v1 = queryTree(arr, rl, rr, 2*node, left, mid);
-	int v2 = queryTree(arr, rl, rr, 2*node+1, mid+1, right);
+	int leftIndex = node*2;
+	int rightIndex = node*2 + 1;
 
-	if(v1 == -1) return v2;
-	if(v2 == -1) return v1;
+	int leftMax = rangeMaxQuery(values, rangeLeft, rangeRight, searchLeft, mid, leftIndex);
+	int rightMax = rangeMaxQuery(values, rangeLeft, rangeRight, mid+1, searchRight, rightIndex);
 
-	return arr[v2] > arr[v1] ? v2 : v1;
+	if(leftMax == -1) return rightMax;
+	if(rightMax == -1) return leftMax;
+
+	int leftValue = values[leftMax];
+	int rightValue = values[rightMax];
+
+	return leftValue > rightValue ? leftMax : rightMax;
 }
 
-int main(){
+int main() {
 	//freopen("input.txt", "r", stdin);
-	int n(0), q(0), e(0), p(0), f(0), pStart(0), r(0), l(0);
+	int n(0), q(0), c(0), p(0), f(0);
 
-	while(scanf("%d %d", &n, &q) == 2){
-		initSegmentTree(n);
+	while(scanf("%d %d", &n, &q) == 2 && n != 0){
+		vector<int> fArr(n);
+		vector<int> values(n);
+		REP(i,0,n-1){
+			scanf("%d", &c);
 
-		REP(i, 0, n-1){
-			scanf("%d", &e);
-			actual[i] = e;
-			if(i == 0){ p = e; f = 0;}
-			if(e == p) f+= 1;
-			if(e != p || i == n-1){
-				for(int j = i-1; j >= pStart; j--){
-					ends[j] = i-1;
-					freq[j] = f;
+			values[i] = c;
+
+			if(i == 0){
+				start[i] = i;
+			}else if(c != p){
+				for(int j = i-1; j >= start[i-1]; j--){
+					fArr[j] = f;
+					elemEnd[j] = i-1;
 				}
-				if(i == n-1){
-					if(e==p) freq[i] = f;
-					else freq[i] = 1;
-				} 
-				f = 1;
-				pStart = i;
+				f = 0;
+				start[i] = i;
+			}else{
+				start[i] = start[i-1];
 			}
-			start[i] = pStart;	
-			p = e;
+			f+=1;
+			p = c;
+
+			if(i == n-1){
+				for(int j = i; j >= start[i]; j--){
+					fArr[j] = f;
+					elemEnd[j] = i;
+				}
+			}
 		}
 
-		buildSegmentTree(freq, 1, 0, n-1);
+		initTree(n);
+		buildTree(fArr,0,n-1, 1);
 
 		REP(i,0,q-1){
-			scanf("%d %d", &l, &r);
-			r--; l--;
-
-			if(actual[l] == actual[r]){
-				printf("%d\n", r-l + 1);
-				continue;
+			//re use p and f
+			scanf("%d %d", &p, &f);
+			f--;p--;
+			//Case 1, same element
+			if(values[p] == values[f]){
+				printf("%d\n", f-p + 1);
 			}
-			int f1 = ends[l] - l + 1;
-			int f2 = r - start[r] + 1;
-			int f3 = -1;
-			if((start[r] - 1) > ends[l]) f3 = freq[queryTree(freq, ends[l] + 1, start[r] - 1, 1, 0, n-1)];
-			printf("%d\n", max(f3,max(f1, f2)));
+
+			else{
+				int segment1 = elemEnd[p] - p + 1;
+				int segment2 = f - start[f] + 1;
+
+				if(start[f] - elemEnd[p] > 1){
+					int segment3 = fArr[rangeMaxQuery(fArr,elemEnd[p] + 1, start[f] - 1, 0, n-1, 1)];
+					printf("%d\n", max(segment3, max(segment1, segment2)));
+				}else{
+					printf("%d\n", (segment2 > segment1 ? segment2 : segment1));
+				}
+			}
 		}
 	}
-
-	return 0;
 }
